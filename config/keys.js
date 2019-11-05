96ff329d33860d0dcb93ba0159ea3071b2b3d1a1
Amit Pathak <ammubhardwaj4@gmail.com>
2019-11-05 16:08:34 +0530

    Add OAuth LogIn App files

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+// add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID:
+      '67825981190-ac7a6l8ga2rtcoeklprh600vu0d28rt7.apps.googleusercontent.com',
+    clientSecret: 'tHmQ2IgLRnAZFfmp7Wg-hZMC'
+  },
+  mongodb: {
+    dbURI:
+      'mongodb+srv://amit:AGQlBjXm5DZ1wUb4@natours-app-u2u4o.mongodb.net/passport-oauth-test'
+  },
+  session: {
+    cookieKey: 'i-love-my-india.'
+  }
+};
